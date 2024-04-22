b252bd0db2f9a0096753da5c9acb604fa641ace1
korken <151646635+kjdutaro@users.noreply.github.com>
2024-04-22 16:09:21 +0800

    card components updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,14 @@ import sample from '../assets/sample.jpg';
 
 export const Card = () => {
   return (
-    <div className="card">
-      <div className="out aspect-square">
-        <img src={sample} alt="image" />
+    <div className="card container">
+      <div className="out aspect-square h-60 rounded-lg">
+        <img className='rounded-lg' src={sample} alt="image" />
       </div>
-      <div className="out flex-auto">
-        <div className="out">Description</div>
-        <div className="out">Price</div>
-        <div className="flex justify-end">
+      <div className="out flex-auto min-w-60">
+        <h1 className="out">Description</h1>
+        <h2 className="out">Price</h2>
+        <div className="out flex justify-end items-end self-end">
           <button>minus</button>
           <input className="border border-gray-300 text-center w-5" type="text" placeholder="1" />
           <button>plus</button>
